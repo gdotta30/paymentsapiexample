844dcc4c3b1d98383da36d9ff1b60bacf1a4640c
German Dotta <gdotta30@gmail.com>
2024-04-29 16:40:27 -0300

    fix indentation

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ class PaymentsApi {
     static internal_errorCode = 0;
 
     static ErrorCode = function() { 
-		return PaymentsApi.internal_errorCode;
-	};
+        return PaymentsApi.internal_errorCode;
+    };
     
     static internal_errorDesc = "";
 
     static ErrorDesc = function() { 
-		return PaymentsApi.internal_errorDesc;
-	};
+        return PaymentsApi.internal_errorDesc;
+    };
     
     static async payWithUI(params) {
-   		var parmsArray = [];
-		parmsArray.push(params);
+        var parmsArray = [];
+        parmsArray.push(params);
         let reference = "";
-		try {
-			reference = window.GxSuperAppApi.call("PayWithUI",parmsArray).then(
+        try {
+            reference = window.GxSuperAppApi.call("PayWithUI",parmsArray).then(
                 function (value) {
                     return value;
                 },
@@ -25,14 +25,14 @@ class PaymentsApi {
                     PaymentsApi.internal_errorCode = 1;
                     PaymentsApi.internal_errorDesc = error;
                     return "";
-			    }
+                }
             );
-		} catch (error) {
-			PaymentsApi.internal_errorCode = 999;
+        } catch (error) {
+            PaymentsApi.internal_errorCode = 999;
             PaymentsApi.internal_errorDesc = error.message;
-		}
+        }
         return reference;
-	};
+    };
 
     static async payWithoutUI(params) {
         var parmsArray = [];
